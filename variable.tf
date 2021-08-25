38a8554633c0d410312138a462706b93cd02c6dc
Danderama <89509957+Danderama@users.noreply.github.com>
2021-08-25 17:53:25 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "AWS_ACCESS_KEY" {
+   default = "AKIA5PAXDC5AJOHFYDUT"
+}
+variable "AWS_SECRET_KEY" {
+   default = "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"
+}
+variable "AWS_REGION" {
+   default = "us-east-1"
+}
